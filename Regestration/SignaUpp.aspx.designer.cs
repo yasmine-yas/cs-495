33f8d5156c2cc40a91e6a3cb007ffb56aafee88d
yasmine muhamed <20192874@fue.edu.eg>
2022-05-22 22:51:23 +0200

    qa

--- Regestration/SignaUpp.aspx.designer.cs
+++ Regestration/SignaUpp.aspx.designer.cs
@@ -375,6 +375,24 @@ namespace Regestration
         protected global::System.Web.UI.WebControls.FileUpload fupPic;
 
         /// <summary>
+        /// Label24 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label24;
+
+        /// <summary>
+        /// ddlRole control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlRole;
+
+        /// <summary>
         /// btnSubmit control.
         /// </summary>
         /// <remarks>
